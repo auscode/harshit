0cfd8d2181c87820f7b0b6b3e9f0f6a0babda4fb
auscode <harshittext@gmail.com>
2024-10-21 22:10:38 +0530

    first commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,92 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "Real Time Chat Translation",
+    description:
+      "It is build on top of the React JS, Node with styledComponents and WebSockets ",
+    tags: ["react", "node", "materialUi", "mongoDB", "webSockets"],
+
+    demo: "https://aurbatao.vercel.app/",
+    github: "https://github.com/auscode/client",
+  },
+  {
+    id: 2,
+    name: "Blogging Website",
+    description:
+      "A Blog website built on React Js, can handle up to 30K users daily.",
+    tags: ["react", "appwrite", "tailwind"],
+
+    demo: "https://lekhan.vercel.app/",
+    github: "https://github.com/auscode/MegaBlog",
+  },
+  {
+    id: 3,
+    name: "DashBoard",
+    description:
+      "An awesome way to visualise and present data. Using React and componental charts",
+    tags: ["react", "chartjs"],
+
+    demo: "https://dashboard-d2.vercel.app/",
+    github: "https://github.com/auscode/dashboard-d2",
+  },
+  {
+    id: 4,
+    name: "Video Streaming Player",
+    description: "Video Streaming Web Application with Admin Panel with age ",
+    tags: ["Django", "CSS", "HTML", "JS", "SQL"],
+
+    demo: "https://github.com/auscode/capstone",
+    github: "https://github.com/auscode/capstone",
+  },
+  {
+    id: 5,
+    name: "Science Inn",
+    description:
+      "A website based on Space technology, Astronomy, Live ISS tracker, Daily Science News & Blogs",
+    tags: ["react", "react-router", "tailwind"],
+
+    demo: "https://scienceinn.vercel.app/",
+    github: "https://github.com/auscode/scienceinn-web",
+  },
+  {
+    id: 6,
+    name: "Responsive Music Portfolio",
+    description:
+      "A music portfolio that shows responsive layout and performance of 3D Cards and Images",
+    tags: ["NextJs", "Tailwind", "AcertinintyUI"],
+
+    demo: "https://musixnextjs.vercel.app/",
+    github: "https://github.com/auscode/musixnextjs",
+  },
+  {
+    id: 7,
+    name: "Snake & Ladder",
+    description:
+      "A Realtime 4 player Snake and Ladder game with spectator feature",
+    tags: ["HTML", "CSS", "JavaScript"],
+
+    demo: "https://snakes-and-ladders-multiplayer.onrender.com/",
+    github: "https://github.com/auscode/snakes-and-ladders-multiplayer",
+  },
+  {
+    id: 8,
+    name: "Old PortFolio",
+    description: "My Old Portfolio on Github on using vanilla",
+    tags: ["HTML", "CSS", "JavaScript"],
+
+    demo: "https://auscode.github.io/Portfolio/",
+    github: "https://github.com/auscode/Portfolio",
+  },
+  {
+    id: 9,
+    name: "Emotional Recommendation System",
+    description:
+      "Suggests Books By analysing the emotional state of user by using AI",
+    tags: ["opencv", "Python", "Django", "TensorFlow"],
+
+    demo: "https://auscode.github.io/Portfolio/",
+    github: "https://github.com/auscode/Portfolio",
+  },
+];
